d8e01dd193e1e165a8e438bacda57298d991029e
JinXx ! <163841101+JinX-7@users.noreply.github.com>
2024-04-06 13:30:29 +0000

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-let router = express.Router();
+let router = express.Router()
 const pino = require("pino");
 const {
     default: makeWASocket,
@@ -9,77 +9,59 @@ const {
     makeCacheableSignalKeyStore
 } = require("@whiskeysockets/baileys");
 
-let isProcessRunning = false; // تتبع حالة التشغيل
-
-function removeFile(FilePath) {
-    if (!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true });
-}
-
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
 router.get('/', async (req, res) => {
-    if (isProcessRunning) {
-        return res.send({ message: "Process is already running" });
-    }
-
-    isProcessRunning = true; // تعيين حالة التشغيل
-
     let num = req.query.number;
-
-    async function XeonPair() {
+        async function XeonPair() {
         const {
             state,
             saveCreds
-        } = await useMultiFileAuthState(`./session`);
-
-        try {
+        } = await useMultiFileAuthState(`./session`)
+     try {
             let XeonBotInc = makeWASocket({
                 auth: {
                     creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
                 },
                 printQRInTerminal: false,
-                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-                browser: ["Ubuntu", "Chrome", "20.0.04"],
-            });
-
-            if (!XeonBotInc.authState.creds.registered) {
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Ubuntu", "Firefox", "20.0.04"],
+             });
+             if(!XeonBotInc.authState.creds.registered) {
                 await delay(1500);
-                num = num.replace(/[^0-9]/g, '');
-                const code = await XeonBotInc.requestPairingCode(num);
-
-                if (!res.headersSent) {
-                    await res.send({ code });
-                }
-            }
-
-            XeonBotInc.ev.on('creds.update', saveCreds);
-
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await XeonBotInc.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            XeonBotInc.ev.on('creds.update', saveCreds)
             XeonBotInc.ev.on("connection.update", async (s) => {
-                const { connection, lastDisconnect } = s;
-
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
                 if (connection == "open") {
-                    await delay(10000);
+                await delay(10000);
                     const sessionXeon = fs.readFileSync('./session/creds.json');
                     const audioxeon = fs.readFileSync('./kongga.mp3');
                     XeonBotInc.groupAcceptInvite("Kjm8rnDFcpb04gQNSTbW2d");
-
-                    // إرسال الملفات والرسائل هنا
-                    if (!res.headersSent) {
-                        const xeonses = await XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` });
-                        XeonBotInc.sendMessage(XeonBotInc.user.id, {
-                            audio: audioxeon,
-                            mimetype: 'audio/mp4',
-                            ptt: true
-                        }, { quoted: xeonses });
-                        await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `_*هاذا الملف خاص باإنشاء بوت جيطوسة وبوبيزة بوت قم بلصق الملف في الخانة الخاصة به*_\n\n*_البوتات المدعومة_*\n
-- _github.com/noureddineouafy/bobizaa_\n
-- _JITOSSA_ _*قادم قريبا...*_\n_©OMARCHARAF1_
-_©noureddineouafy_` }, { quoted: xeonses });
-                        await delay(100);
-                        await removeFile('./session');
-                        process.exit(0);
-                    }
-                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+				const xeonses = await XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` });
+				XeonBotInc.sendMessage(XeonBotInc.user.id, {
+                    audio: audioxeon,
+                    mimetype: 'audio/mp4',
+                    ptt: true
+                }, {
+                    quoted: xeonses
+                });
+				await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `_*انت قريب من أن تصنع البوت الخاص بك*_\n_قم بنسخ محتوى الملف  cards.json قوم بلصقه في الfork الخاص بيك في github/JitossaSession_\n\n instagram\n instagram.com/ovmar_1\n telegram\n @Jinkx7\n whatsapp\n+212670941551\n\n ©JITOSSA-OMAR` }, {quoted: xeonses});
+        await delay(100);
+        return await removeFile('./session');
+        process.exit(0)
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
                     XeonPair();
                 }
@@ -87,27 +69,24 @@ _©noureddineouafy_` }, { quoted: xeonses });
         } catch (err) {
             console.log("service restated");
             await removeFile('./session');
-            if (!res.headersSent) {
-                await res.send({ code: "Service Unavailable" });
-            }
-        } finally {
-            isProcessRunning = false; // إعادة تعيين حالة التشغيل بعد الانتهاء من العملية
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
         }
     }
-
-    return await XeonPair();
+    return await XeonPair()
 });
 
 process.on('uncaughtException', function (err) {
-    let e = String(err);
-    if (e.includes("conflict")) return;
-    if (e.includes("Socket connection timeout")) return;
-    if (e.includes("not-authorized")) return;
-    if (e.includes("rate-overlimit")) return;
-    if (e.includes("Connection Closed")) return;
-    if (e.includes("Timed Out")) return;
-    if (e.includes("Value not found")) return;
-    console.log('Caught exception: ', err);
-});
+let e = String(err)
+if (e.includes("conflict")) return
+if (e.includes("Socket connection timeout")) return
+if (e.includes("not-authorized")) return
+if (e.includes("rate-overlimit")) return
+if (e.includes("Connection Closed")) return
+if (e.includes("Timed Out")) return
+if (e.includes("Value not found")) return
+console.log('Caught exception: ', err)
+})
 
-module.exports = router;
+module.exports = router
